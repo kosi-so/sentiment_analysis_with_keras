eab06ae6f7fd70b6f83d58e38865327853af2bf7
Kosi Akaeze <kossyfab@gmail.com>
2022-12-23 12:42:41 +1100

    first commit

---
+++ setup.py
@@ -0,0 +1,10 @@
+from setuptools import find_packages, setup
+
+setup(
+    name='src',
+    packages=find_packages(),
+    version='0.1.0',
+    description='sentiment analysis using a keras model',
+    author='kosi_so',
+    license='',
+)
